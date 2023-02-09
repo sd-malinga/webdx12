e73b602a84927c3f1a8deda728ab5cf5c62b8dc6
Devign Experience <devignx@gmail.com>
2023-02-09 22:46:47 +0530

    3rd commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,16 +4,30 @@ import pay from '../assets/div.webp'
 import vid from '../assets/vid.webp'
 import img1 from '../assets/img-1.webp'
 import img2 from '../assets/img-2.webp'
+import mob from '../assets/mobile.webp'
 import shp1 from '../assets/shp01.svg'
+import shp2 from '../assets/shp02.svg'
+
+import gp from '../assets/gp.png'
+import as from '../assets/as.png'
+
+import fe1 from '../assets/features/feat01.webp'
+import fe2 from '../assets/features/feat02.webp'
+import fe3 from '../assets/features/feat03.webp'
+import fe4 from '../assets/features/feat04.webp'
+import fe5 from '../assets/features/feat05.webp'
+import fe6 from '../assets/features/feat06.webp'
+
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return(
         <div>
-            <section id='nav' className="flex fixed backdrop-blur-xl w-full top-0 bg-white/90 toppp justify-center md:justify-between p-6 px-12 items-center">
-                <img className='h-[1rem] lg:h-auto' src={logo} />
+            <section id='nav' className="flex fixed backdrop-blur-xl w-full top-0 bg-white/90 topppp justify-center md:justify-between p-6 px-12 items-center">
+                <a href='#home'><img className='h-[1rem] lg:h-auto' src={logo} /></a>
                 <div className="pc hidden lg:visible text-black/60 font-medium text-md flex lg:gap-10">
-                    <a href="/" className="hover:text-blue-800 transition-all duration-500 ease-in-out ">About</a>
-                    <a href="/" className="hover:text-blue-800 transition-all duration-500 ease-in-out ">Services</a>
+                    <a href="#about" className="hover:text-blue-800 transition-all duration-500 ease-in-out ">About</a>
+                    <a href="#services" className="hover:text-blue-800 transition-all duration-500 ease-in-out ">Services</a>
                     <a href="/" className="hover:text-blue-800 transition-all duration-500 ease-in-out ">Roadmap</a>
                     <a href="/" className="hover:text-blue-800 transition-all duration-500 ease-in-out ">Statistic</a>
                     <a href="/" className="hover:text-blue-800 transition-all duration-500 ease-in-out ">Token</a>
@@ -24,7 +38,8 @@ const Home = () => {
                 </div>
                 <button className='rounded-lg pc px-6 py-4 text-white font-medium text-sm grad'>Join Brit World</button>
             </section>
-            <section className="mt-[15%] lg:mt-[8%] relative">
+
+            <section id='#home' className="mt-[15%] lg:mt-[8%] relative">
                 <h1 className="text-black/90 w-10/12 lg:w-[60%] m-auto pt-16 text-center font-black text-[2rem] md:text-[3rem] leading-[2rem] md:leading-[3rem] lg:leading-[3.5rem]">
                 Brit Gold Just Entered<br/>
                 <span className='font-[100] text-[1.7rem] md:text-[2.7rem] text-black/60'>the Real World</span>
@@ -43,12 +58,11 @@ const Home = () => {
                     <button className='py-4 px-8 bg-blue-200 w-11/12 lg:w-fit hover:bg-blue-600 hover:drop-shadow-xl transition-all duration-500 ease-in-out rounded-md text-white shad'>Whitepapers</button>
                 </div>
                 <img className='m-auto mt-12 noselect' src={pay} />
-
             </section>
 
             <img className='m-auto w-11/12 mt-12 drop-shadow-xl shad hover:p-4 bg-white/20 backdrop-blur-xl hover:scale-[102%] transition-all duration-700 ease-in-out rounded-lg  md:w-1/2' src={vid} />
 
-            <section id="economy" className="mt-8 md:mt-16 relative w-full gap-8 px-3 lg:px-12 flex flex-col-reverse lg:flex-row toppp">
+            <section id="#economy" className="mt-8 md:mt-16 relative w-full gap-8 px-3 lg:px-12 flex flex-col-reverse lg:flex-row toppp">
                 <p className='absolute mob top-8 m-auto left-12 md:left-0  text-xs text-blue-500'>▽ DECENTRALISED ECONOMY</p>
                 <p className='absolute pc top-8 m-auto right-16  text-xs text-blue-500'>▽ DECENTRALISED ECONOMY</p>
                 <img className='p-6 object-contain' src={img1} />
@@ -89,7 +103,7 @@ const Home = () => {
                 </div>
             </section>
 
-            <section id="about" className="mt-8 md:mt-16 relative w-full gap-8 px-3 lg:px-12 flex flex-col-reverse lg:flex-row-reverse toppp">
+            <section id="#about" className="mt-8 md:mt-16 relative w-full gap-8 px-3 lg:px-12 flex flex-col-reverse lg:flex-row-reverse toppp">
                 <p className='absolute mob top-8 m-auto left-12 md:left-0 bottt text-xs text-blue-500'>▽ ABOUT BRIT GOLD</p>
                 <p className='absolute pc top-8 m-auto ml-4 left-24 bottt  text-xs text-blue-500'>▽ ABOUT BRIT GOLD</p>
                 <img className='p-6 object-contain' src={img2} />
@@ -130,6 +144,75 @@ const Home = () => {
                 </div>
             </section>
 
+            <section id='#services' className='mt-8 md:mt-16 relative'>
+                <p className='absolute mob top-8 m-auto left-12 md:left-0 bottt text-xs text-blue-500'>▽ ABOUT BRIT GOLD</p>
+                <p className='absolute pc top-8 m-auto centerh bottt  text-xs text-blue-500'>▽ ABOUT BRIT GOLD</p>
+                <h1 className="text-black/90 w-10/12 lg:w-[60%] m-auto pt-16 text-center font-black text-[2rem] md:text-[3rem] leading-[2rem] md:leading-[3rem] lg:leading-[3.5rem]">Brit Gold Features</h1>
+
+                <div className='w-full md:w-[85%] m-auto mt-6 scale-[90%] md:scale-[85%] justify-center flex flex-wrap gap-4'>
+                    <div className='flex flex-wrap md:flex-nowrap justify-center gap-4 bg-white rounded-xl p-4 items-start w-full md:w-[45%]'>
+                        <img className=' object-contain p-2 shad1 rounded-full' src={fe1} />
+                        <div className='text-center md:text-left '>
+                            <h3 className=' font-bold text-2xl my-2'>General ecosystem</h3>
+                            <p className='text-black/60'>Surf sardine mooneye, sawtooth eel trout-perch, Death Valley pupfish zebra tilapia medaka golden shiner, danio Black angelfish sandperch, whiff.</p>
+                        </div>
+                    </div>
+                    <div className='flex flex-wrap md:flex-nowrap justify-center gap-4 bg-white rounded-xl p-4 items-start w-full md:w-[45%]'>
+                        <img className=' object-contain p-2 shad1 rounded-full' src={fe2} />
+                        <div className='text-center md:text-left '>
+                            <h3 className=' font-bold text-2xl my-2'>General ecosystem</h3>
+                            <p className='text-black/60'>Surf sardine mooneye, sawtooth eel trout-perch, Death Valley pupfish zebra tilapia medaka golden shiner, danio Black angelfish sandperch, whiff.</p>
+                        </div>
+                    </div>
+                    <div className='flex flex-wrap md:flex-nowrap justify-center gap-4 bg-white rounded-xl p-4 items-start w-full md:w-[45%]'>
+                        <img className=' object-contain p-2 shad1 rounded-full' src={fe3} />
+                        <div className='text-center md:text-left '>
+                            <h3 className=' font-bold text-2xl my-2'>Exchange for other crypto currency</h3>
+                            <p className='text-black/60'>Surf sardine mooneye, sawtooth eel trout-perch, Death Valley pupfish zebra tilapia medaka golden shiner, danio Black angelfish sandperch, whiff.</p>
+                        </div>
+                    </div>
+                    <div className='flex flex-wrap md:flex-nowrap justify-center gap-4 bg-white rounded-xl p-4 items-start w-full md:w-[45%]'>
+                        <img className=' object-contain p-2 shad1 rounded-full' src={fe4} />
+                        <div className='text-center md:text-left '>
+                            <h3 className=' font-bold text-2xl my-2'>Protection against fraud</h3>
+                            <p className='text-black/60'>Surf sardine mooneye, sawtooth eel trout-perch, Death Valley pupfish zebra tilapia medaka golden shiner, danio Black angelfish sandperch, whiff.</p>
+                        </div>
+                    </div>
+                    <div className='flex flex-wrap md:flex-nowrap justify-center gap-4 bg-white rounded-xl p-4 items-start w-full md:w-[45%]'>
+                        <img className=' object-contain p-2 shad1 rounded-full' src={fe5} />
+                        <div className='text-center md:text-left '>
+                            <h3 className=' font-bold text-2xl my-2'>Transfer to Debit card</h3>
+                            <p className='text-black/60'>Surf sardine mooneye, sawtooth eel trout-perch, Death Valley pupfish zebra tilapia medaka golden shiner, danio Black angelfish sandperch, whiff.</p>
+                        </div>
+                    </div>
+                    <div className='flex flex-wrap md:flex-nowrap justify-center gap-4 bg-white rounded-xl p-4 items-start w-full md:w-[45%]'>
+                        <img className=' object-contain p-2 shad1 rounded-full' src={fe6} />
+                        <div className='text-center md:text-left '>
+                            <h3 className=' font-bold text-2xl my-2'>Anonymity</h3>
+                            <p className='text-black/60'>Surf sardine mooneye, sawtooth eel trout-perch, Death Valley pupfish zebra tilapia medaka golden shiner, danio Black angelfish sandperch, whiff.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className='mt-0 md:mt-8 relative'>
+                <p className='absolute mob top-8 m-auto left-12 md:left-0 bottt text-xs text-blue-500'>▽ APPS</p>
+                <p className='absolute pc top-8 m-auto ml-4 left-16 bottt  text-xs text-blue-500'>▽ APPS</p>
+                <div className='relative w-full h-[60vh] overflow-hidden'>
+                    <img className='absolute bottt pc right-0 w-1/2' src={shp2} />
+                    <div className='flex flex-col justify-center ml-[8%] md:ml-[6%]'>
+                        <h1 className="text-black/90 w-11/12 lg:w-[40%] pt-16 font-black text-[2rem] md:text-[3rem] leading-[2rem] md:leading-[3rem] lg:leading-[3.5rem]">
+                        Brit Gold App for ICO Platform
+                        </h1>
+                        <p className='text-black/50 w-11/12 lg:w-[50%]  mt-4  text-lg '>JavaScript virtual machines (VMs) and platforms built upon them have also increased the popularity of JavaScript for server-side web applications. On the client side, JavaScript</p>
+                        <div className='flex w-10/12 gap-3 md:gap-6 mt-8'>
+                            <img className='w-[45%] md:w-auto' src={gp} />
+                            <img className='w-[45%] md:w-auto'  src={as} />
+                        </div>
+                        <img className='absolute pc -bottom-8 right-[20%] w-1/4' src={mob} />
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
