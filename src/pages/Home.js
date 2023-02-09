6c9b0c65bd14d168d77633b0c157f1e4ca366ebb
Devign Experience <devignx@gmail.com>
2023-02-09 20:31:06 +0530

    1st offl commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,35 +1,46 @@
-import img1 from '../assets/blob.webp'
+import logo from '../assets/head-logo.webp'
+import logor from '../assets/logo-round.webp'
+import pay from '../assets/div.webp'
+import shp1 from '../assets/shp01.svg'
 
 const Home = () => {
-
     return(
         <div>
-            
-            <div class="opacity-40 w-[200px] md:w-[350px] h-[350px] bg-red-300 rounded-full blur-[140px] bottt top-[35%] left-0 absolute "></div>
-            <div class="opacity-40 w-[200px] md:w-[350px] h-[350px] bg-blue-300 rounded-full blur-[140px] bottt top-[35%] right-0 absolute "></div>
-    
-            <section id='nav' className="flex backdrop-blur-xl toppp justify-center md:justify-between p-8 px-12 items-center">
-                <p className="text-blue-800 font-bold text-2xl">$LOGO</p>
-                <div className="pc text-blue-800/60 font-medium text-lg flex md:gap-8 lg:gap-12">
+            <section id='nav' className="flex fixed backdrop-blur-xl w-full top-0 bg-white/90 toppp justify-center md:justify-between p-6 px-12 items-center">
+                <img className='h-[1rem] lg:h-auto' src={logo} />
+                <div className="pc hidden lg:visible text-black/60 font-medium text-md flex lg:gap-10">
                     <a href="/" className="hover:text-blue-800 transition-all duration-500 ease-in-out ">About</a>
                     <a href="/" className="hover:text-blue-800 transition-all duration-500 ease-in-out ">Services</a>
                     <a href="/" className="hover:text-blue-800 transition-all duration-500 ease-in-out ">Roadmap</a>
                     <a href="/" className="hover:text-blue-800 transition-all duration-500 ease-in-out ">Statistic</a>
                     <a href="/" className="hover:text-blue-800 transition-all duration-500 ease-in-out ">Token</a>
+                    <a href="/" className="hover:text-blue-800 transition-all duration-500 ease-in-out ">Whitepaper</a>
                     <a href="/" className="hover:text-blue-800 transition-all duration-500 ease-in-out ">Team</a>
                     <a href="/" className="hover:text-blue-800 transition-all duration-500 ease-in-out ">FAQ</a>
+                    <a href="/" className="hover:text-blue-800 transition-all duration-500 ease-in-out ">Blog</a>
                 </div>
+                <button className='rounded-lg pc px-6 py-4 text-white font-medium text-sm grad'>Join Brit World</button>
             </section>
-
-            <section className="mt-[10%] relative">
-                <h1 className="text-black/70 w-10/12 md:w-[60%] m-auto text-center font-black text-2xl md:text-4xl">
-                    Lorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur adip
+            <section className="mt-[15%] lg:mt-[8%] relative">
+                <h1 className="text-black/90 w-10/12 lg:w-[60%] m-auto pt-16 text-center font-black text-[2rem] md:text-[3rem] leading-[2rem] md:leading-[3rem] lg:leading-[4rem]">
+                Brit Gold Just Entered<br/>
+                <span className='font-[100] text-[1.7rem] md:text-[2.7rem] text-black/60'>the Real World</span>
                 </h1>
-                <div className='scale-[50%] md:scale-[100%] blur-lg md:w-[150px] toppp h-[150px] mix-blend-overlay bg-white rounded-full drop-shadow-2xl absolute top-1/2 right-1/2'></div>
-                <img className='m-auto w-[40rem] mt-8 blur-xl' src={img1} />
+
+                <div className='pc botttt transition-all duration-500 ease-in-out '>
+                    <div className='scale-[50%] md:scale-[100%] md:w-[400px] h-[400px] grad absolute rounded-full drop-shadow-2xl absolute top-4 -left-[8%]'></div>
+                    <div className='scale-[50%] md:scale-[100%] md:w-[150px] h-[150px] bg-white rounded-full drop-shadow-2xl absolute top-[28%] left-[12%]'></div>
+                <img className='absolute right-0 top-[10%] ' src={shp1} />
+                </div>
+
+                <div className='m-auto w-full mt-16 flex gap-3 items-center lg:gap-8 flex-col lg:flex-row justify-center'>
+                    <button className='py-4 px-8 grad rounded-md w-11/12 lg:w-fit hover:drop-shadow-xl transition-all duration-500 ease-in-out text-white shad'>Buy Tokens 45% Off</button>
+                    <button className='py-4 px-8 bg-blue-200 w-11/12 lg:w-fit hover:bg-blue-600 hover:drop-shadow-xl transition-all duration-500 ease-in-out rounded-md text-white shad'>Whitepapers</button>
+                </div>
+                <img className='m-auto mt-12 noselect' src={pay} />
             </section>
 
-            <section id="about" className="my-8">
+            <section id="about" className="mt-16 toppp">
                 <h1 className="text-blue-800 w-10/12 md:w-[60%] m-auto text-center font-black text-2xl md:text-4xl "><a className='transition-all duration-500 ease-in-out hover:text-blue-700' href='#about'>About</a></h1>
                 <p className="text-black/50 w-10/12  md:w-[60%] mt-4 md:mt-8 m-auto text-center text-lg md:text-xl">
                     Lorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur adip
@@ -41,3 +52,4 @@ const Home = () => {
 }
 
 export default Home;
+<img className='m-auto w-[40rem] mt-8 ' src={logor} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        blue: {
+          200:'#557EF8',
+        }
+      }
+    },
   },
   plugins: [],
 }
